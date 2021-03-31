068d87f2b9963ac5174d9b944f1aed9156753503
miyukiccina <80190927+miyukiccina@users.noreply.github.com>
2021-03-31 21:52:25 +0900

    Add files via upload

--- italian_restaurant.sql
+++ italian_restaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 25, 2021 at 10:52 AM
+-- Generation Time: Mar 31, 2021 at 12:51 PM
 -- Server version: 5.7.32
 -- PHP Version: 7.4.12
 
@@ -41,10 +41,10 @@ CREATE TABLE `italian_restaurant` (
 --
 
 INSERT INTO `italian_restaurant` (`number`, `name`, `area`, `area2`, `category`, `adress`, `comment`) VALUES
-(1, 'test1', 'test1', 'test1', 'test1', 'test1', 'test1'),
-(2, 'test2', 'test2', 'test2', 'test2', 'test2', 'test2'),
-(3, 'test3', 'test3', 'test3', 'test3', 'test3', 'test3'),
-(4, 'Dal Brigante', 'Osaka', 'Fukushima', 'Pizza e gli altri', 'Osaka Fukushima 0-0-0', 'Ottima pizza che si può trovare a Osaka');
+(4, 'Dal Brigante', 'test3', 'test3', 'test3', 'test3', 'test3'),
+(8, 'La Romantica', 'Osaka', 'Nakazaki-cho', 'Pizza e gli altri', 'Osaka Nakazakicho 0-0-0', 'ottimo'),
+(9, 'Medditeranea', 'Tokyo', 'Hamamatsucho', 'Colazione', 'Tokyo Minato-ku 0-0-0', 'si può prendere la colazione italiana'),
+(10, 'あ', 'あ', 'あ', 'あ', 'あ', 'あ');
 
 --
 -- Indexes for dumped tables
@@ -64,7 +64,7 @@ ALTER TABLE `italian_restaurant`
 -- AUTO_INCREMENT for table `italian_restaurant`
 --
 ALTER TABLE `italian_restaurant`
-  MODIFY `number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
